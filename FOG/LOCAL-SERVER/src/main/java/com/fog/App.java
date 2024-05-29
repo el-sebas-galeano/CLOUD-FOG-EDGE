9150ef1683b1f3670bbb6156c4fc375966b9ab87
JuanSebastianGaleanoGonzalez <71532153+JuanSebastianGaleanoGonzalez@users.noreply.github.com>
2024-05-29 07:54:26 -0500

    Mediciones Incompleto

--- FOG/LOCAL-SERVER/src/main/java/com/fog/App.java
+++ FOG/LOCAL-SERVER/src/main/java/com/fog/App.java
@@ -1,11 +1,17 @@
 package com.fog;
 
+import java.util.ArrayList;
+
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
 
-public class App 
-{
+public class App {
+
+    ArrayList<String> medicionesHumo = new ArrayList<>();
+    ArrayList<String> medicionesTemperatura = new ArrayList<>();
+    ArrayList<String> medicionesHumedad = new ArrayList<>();    
+
     public static void main( String[] args ){
         try(ZContext context = new ZContext()){
             Socket socketProxy = context.createSocket(SocketType.PULL);
@@ -17,5 +23,9 @@ public class App
                 System.out.println(data);
             }
         }
+    }
+
+    public void evaluarMedicion(String medicion){
+        
     }
 }
