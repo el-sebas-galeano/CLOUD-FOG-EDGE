9d9367a10cc3f6266d92e6771a1b511f74a284d2
JuanSebastianGaleanoGonzalez <71532153+JuanSebastianGaleanoGonzalez@users.noreply.github.com>
2024-05-29 03:15:42 -0500

    Load Balancer - Proxy - Conexion con edge sensor humo

--- FOG/LOAD-BALANCER/src/main/java/com/fog/App.java
+++ FOG/LOAD-BALANCER/src/main/java/com/fog/App.java
@@ -1,13 +1,34 @@
 package com.fog;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.ArrayList;
+import java.util.List;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+
+public class App {
+
+    private static final int PUERTO_EDGE = 5120;
+    private static final int PUERTO_FOG = 5200;
+    public static void main( String[] args ){
+        try (ZContext context = new ZContext()) {
+            Socket pullSocket = context.createSocket(SocketType.PULL);
+            pullSocket.bind("tcp://localhost:" + PUERTO_EDGE);
+
+            Socket pushSocket = context.createSocket(SocketType.PUSH);
+            pushSocket.connect("tcp://localhost:" + PUERTO_FOG);
+
+            while (true) {
+                System.out.println("Esperando.......");
+                String data = pullSocket.recvStr();
+                System.out.println("Received data: " + data);
+                pushSocket.send(data);
+                System.out.println("Sending data: " + data);
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
+
