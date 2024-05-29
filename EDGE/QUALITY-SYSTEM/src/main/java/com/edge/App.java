4b82ecc7edb14a57f8551adee393758619e85f13
Daniel Florido <d.florido@javeriana.edu.co>
2024-05-29 07:02:07 -0500

    Update App.java

--- EDGE/QUALITY-SYSTEM/src/main/java/com/edge/App.java
+++ EDGE/QUALITY-SYSTEM/src/main/java/com/edge/App.java
@@ -10,11 +10,14 @@ public class App
     {
         try(ZContext context = new ZContext()){
             Socket socketCalidad=context.createSocket(SocketType.REP);
+            Socket socketProxy = context.createSocket(SocketType.PUSH);
             socketCalidad.bind("tcp://localhost:5110");
+            socketProxy.connect("tcp://localhost:5120");
             System.out.println("Esperando Alarmas!...");
             while (!Thread.currentThread().isInterrupted()) {
                 String alarma = socketCalidad.recvStr();
                 System.out.println(alarma);
+                socketProxy.send(alarma);
                 socketCalidad.send("Alarma registrada con exito!");
             }
         }
