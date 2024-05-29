7cbb16044c1f1d71956b05cf2dd37be1204eba5f
Daniel Florido <d.florido@javeriana.edu.co>
2024-05-29 07:37:35 -0500

    add balancer port

--- FOG/QUALITY-SYSTEM/src/main/java/com/fog/App.java
+++ FOG/QUALITY-SYSTEM/src/main/java/com/fog/App.java
@@ -4,19 +4,13 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
 
+import com.fog.controller.ControllerAlarmasBalancer;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        try(ZContext context = new ZContext()){
-            Socket socketCalidad=context.createSocket(SocketType.REP);
-            socketCalidad.bind("tcp://localhost:5210");
-            System.out.println("Esperando Alarmas!...");
-            while (!Thread.currentThread().isInterrupted()) {
-                String alarma = socketCalidad.recvStr();
-                System.out.println(alarma);
-                socketCalidad.send("Alarma registrada con exito!");
-            }
-        }
+        ControllerAlarmasBalancer controllerAlarmasBalancer = new ControllerAlarmasBalancer();
+        new Thread(controllerAlarmasBalancer).start();
     }
 }
---
+++ FOG/QUALITY-SYSTEM/src/main/java/com/fog/controller/ControllerAlarmasBalancer.java
@@ -0,0 +1,26 @@
+package com.fog.controller;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ.Socket;
+
+public class ControllerAlarmasBalancer implements Runnable{
+
+    @Override
+    public void run() {
+        try(ZContext context = new ZContext()){
+            Socket socketEnviador = context.createSocket(SocketType.PUSH); 
+            Socket socketRecibidor = context.createSocket(SocketType.REP);
+            socketEnviador.connect("tcp:/localhost:5120");
+            socketRecibidor.bind("tcp:/localhost:5130");
+            System.out.println("Esperando Alarmas!...");
+            while(!Thread.currentThread().isInterrupted()){
+                String alarma = socketRecibidor.recvStr();
+                System.out.println(alarma);
+                socketEnviador.send(alarma);
+                socketRecibidor.send("Alarma registrada con exito!");
+            }
+        }
+    }
+    
+}
