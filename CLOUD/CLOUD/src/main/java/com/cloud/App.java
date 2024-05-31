f6b70bb52c9885982b915c75cee28c3c7984bd07
Daniel Florido <d.florido@javeriana.edu.co>
2024-05-31 17:41:59 -0500

    Update App.java

--- CLOUD/CLOUD/src/main/java/com/cloud/App.java
+++ CLOUD/CLOUD/src/main/java/com/cloud/App.java
@@ -10,14 +10,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        Logger file = new Logger("data.txt");
+        Logger fileAlarmas = new Logger("alarmas.txt");
+        Logger fileMediciones = new Logger("mediciones.txt");
+        Logger fileCalculos = new Logger("calculos.txt");
         try(ZContext context = new ZContext()){
             Socket socketCloud = context.createSocket(SocketType.REP);
             socketCloud.bind("tcp://localhost:5230");
             System.out.println("Esperando mensajes del proxy!...");
             while (!Thread.currentThread().isInterrupted()) {
                 String data = socketCloud.recvStr();
-                file.log(data);
+                if(data.startsWith("A")){
+                    fileAlarmas.log(data);
+                }else if(data.startsWith("M")){
+                    fileMediciones.log(data);
+                }else if(data.startsWith("C")){
+                    fileCalculos.log(data);
+                }else{
+                    System.out.println("Mensaje no reconocido: "+data);
+                }
                 socketCloud.send("Data guardada con exito en cloud!...");
             }
         }
