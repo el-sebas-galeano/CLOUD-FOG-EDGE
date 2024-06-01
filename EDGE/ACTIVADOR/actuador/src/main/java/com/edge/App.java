5cb1107c522c5336bf24e80312c51543c08f3300
Daniel Florido <d.florido@javeriana.edu.co>
2024-06-01 01:16:19 -0500

    add directions

--- EDGE/ACTIVADOR/actuador/src/main/java/com/edge/App.java
+++ EDGE/ACTIVADOR/actuador/src/main/java/com/edge/App.java
@@ -5,13 +5,15 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Socket;
 
+import com.models.direcciones.Direcciones;
+
 public class App 
 {
     public static void main( String[] args )
     {
         try(ZContext context = new ZContext()){
             Socket socketPull = context.createSocket(SocketType.PULL);
-            socketPull.bind("tcp://*:5100");
+            socketPull.bind("tcp://*:"+Direcciones.PUERTO_ACTIVADOR_SENSOR);
             while (!Thread.currentThread().isInterrupted()) {
                 System.out.println("Esperando sensor de humo!...");
                 String message = socketPull.recvStr();
