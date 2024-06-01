092c7c9a386aeefe07198c539d0a456d7ce3aa15
Daniel Florido <d.florido@javeriana.edu.co>
2024-06-01 00:56:42 -0500

    add models

---
+++ MODELS/models/src/main/java/com/models/direcciones/Direcciones.java
@@ -0,0 +1,28 @@
+package com.models.direcciones;
+
+public class Direcciones {
+    public static String DIRECCION_IP_ACTUADOR="10.43.101.36";
+    public static String DIRECCION_IP_SENSORES="10.43.101.36";
+    public static String DIRECCION_IP_EDGE_CALIDAD="10.43.101.36";
+
+    public static String DIRECCION_IP_MONITOR="10.43.100.126";
+    public static String DIRECCION_IP_LOAD_BALANCER="10.43.100.126";
+    public static String DIRECCION_IP_PROXYA="10.43.100.126";
+    public static String DIRECCION_IP_PROXYB="";
+    public static String DIRECCION_IP_LOCAL_SERVER="10.43.100.126";
+    public static String DIRECCION_IP_FOG_CALIDAD="10.43.100.126";
+
+    public static String DIRECCION_IP_CLOUD="10.43.100.130";
+    public static String DIRECCION_IP_CLOUD_CALIDAD="10.43.100.130";
+    
+    public static String PUERTO_ACTIVADOR_SENSOR= "5100";
+    public static String PUERTO_EDGECALIDAD_SENSOR= "5110";
+    public static String PUERTO_CLOUDCALIDAD_CLOUD= "5300";
+    public static String PUERTO_LOAD_BALANCER_PULL= "5120";
+    public static String PUERTO_PROXY_BALANCER_PUSH= "5200";
+    public static String PUERTO_PROXY_LSERVER= "5220";
+    public static String PUERTO_PROXY_CLOUD= "5230";
+    public static String PUERTO_LSERVER_FOGCALIDAD= "5130";
+    public static String PUERTO_MONITOR_PROXY_BEAT= "5400";
+    public static String PUERTO_MONITOR_PROXY_NOTIFICACION= "5410";
+}
---
+++ MODELS/models/src/main/java/com/rangos/Rangos.java
@@ -0,0 +1,8 @@
+package com.rangos;
+
+public class Rangos {
+    public static String MIN_TEMPERATURA="11";
+    public static String MAX_TEMPERATURA="29.4";
+    public static String MIN_HUMEDAD="70";
+    public static String MAX_HUMEDAD="100";
+}
