501a343b087fe51b4f67a887c08af510af21d685
Daniel Florido <d.florido@javeriana.edu.co>
2024-06-01 01:13:49 -0500

    add directions

--- EDGE/QUALITY-SYSTEM/src/main/java/com/edge/App.java
+++ EDGE/QUALITY-SYSTEM/src/main/java/com/edge/App.java
@@ -4,6 +4,8 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
 
+import com.models.direcciones.Direcciones;
+
 public class App 
 {
     public static void main( String[] args )
@@ -11,8 +13,8 @@ public class App
         try(ZContext context = new ZContext()){
             Socket socketCalidad=context.createSocket(SocketType.REP);
             Socket socketProxy = context.createSocket(SocketType.PUSH);
-            socketCalidad.bind("tcp://*:5110");
-            socketProxy.connect("tcp://localhost:5120");
+            socketCalidad.bind("tcp://*:"+Direcciones.PUERTO_EDGECALIDAD_SENSOR);
+            socketProxy.connect("tcp://"+Direcciones.DIRECCION_IP_LOAD_BALANCER+":"+Direcciones.PUERTO_LOAD_BALANCER_PULL);
             System.out.println("Esperando Alarmas!...");
             while (!Thread.currentThread().isInterrupted()) {
                 String alarma = socketCalidad.recvStr();
